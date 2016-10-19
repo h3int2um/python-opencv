498035c2ab907a9934994ebb2a67c660a986f4d4
h3int2um <h3int2um@gmail.com>
2016-10-20 03:36:49 +0700

    Install OpenCV on Ubuntu respository

---
+++ source-code/install_opencv_ubuntu_repository.sh
@@ -0,0 +1,9 @@
+# Install OpenCV on Ubuntu repository
+# References: OpenCV Computer Vision with Python,Joseph Howse, Packt Publishing, April 2013
+# Author: Thi Minh Nhut - Date: October 12, 2016
+
+sudo apt-get update
+sudo apt-get install python-numpy
+sudo apt-get install python-scipy
+sudo apt-get install libopencv-*
+sudo apt-get install python-opencv
